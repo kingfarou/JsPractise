f2ca0a256c8a9889190e60aab32b747a3dc0455e
Kingfar Ou <kingfarou@163.com>
2019-09-14 11:37:31 +0800

    完善注释

--- script/moveElement.js
+++ script/moveElement.js
@@ -2,8 +2,8 @@
  * 将元素放置到指定位置
  * 
  * @param {string} elementId 要放置的元素id
- * @param {int} destinationX 位置，x轴坐标，单位：px
- * @param {int} destinationY 位置，y轴坐标，单位：px
+ * @param {int} destinationX 指定位置的x轴坐标，单位：px
+ * @param {int} destinationY 指定位置的y轴坐标，单位：px
  */
 function positionElement(elementId, destinationX, destinationY){
     var element = document.getElementById(elementId);
@@ -20,8 +20,8 @@ function positionElement(elementId, destinationX, destinationY){
  * 平移（通过动画）元素到指定位置
  * 
  * @param {string} elementId 要移动的元素id
- * @param {int} destinationX 终点位置，x轴坐标
- * @param {int} destinationY 终点位置，y轴坐标
+ * @param {int} destinationX 指定位置的x轴坐标，单位：px
+ * @param {int} destinationY 指定位置的y轴坐标，单位：px
  * @param {int} intervalTime 间隔时间，不是动画的总时长，这是“每动一下”的时间，单位：毫秒
  */
 function moveElement(elementId, destinationX, destinationY, intervalTime){
